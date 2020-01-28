c892fefa9e140157b6fccd9437f65697332ca284
Martin Röbke <martin.roebke@web.de>
2020-01-28 21:35:11 +0100

    Update mpl_test.py
    added "main" check

--- satvisualization_repo/satvisu/mpl_test.py
+++ satvisualization_repo/satvisu/mpl_test.py
@@ -8,14 +8,15 @@ Created on Tue Jan 28 18:52:35 2020
 import matplotlib.image as mpimg
 import matplotlib.pyplot as plt
 
-img=mpimg.imread("incidenceGraph.png")
-plt.axis("off")
-fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(6,12), dpi=600, frameon=False, tight_layout=True)
-for elem in (ax1, ax2):
-    elem.axes.get_xaxis().set_visible(False)
-    elem.axes.get_yaxis().set_visible(False)
-print(fig.get_size_inches())
-ax1.imshow(img)
-ax2.imshow(mpimg.imread("g41Digraph.png"))
-
-plt.show()
+if __name__=="__main__":
+    img=mpimg.imread("incidenceGraph.png")
+    plt.axis("off")
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(6,12), dpi=600, frameon=False, tight_layout=True)
+    for elem in (ax1, ax2):
+        elem.axes.get_xaxis().set_visible(False)
+        elem.axes.get_yaxis().set_visible(False)
+    print(fig.get_size_inches())
+    ax1.imshow(img)
+    ax2.imshow(mpimg.imread("g41Digraph.png"))
+    
+    plt.show()
