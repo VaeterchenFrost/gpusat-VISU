19b1638525b888a6890026d878ef7205f0e435e5
VaeterchenFrost <martin.roebke@web.de>
2020-01-31 19:21:36 +0100

    minimal adjustments Laptop

--- satvisualization_repo/satvisu/mpl_test.py
+++ satvisualization_repo/satvisu/mpl_test.py
@@ -9,16 +9,24 @@ import matplotlib.image as mpimg
 import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
-    img = mpimg.imread("incidenceGraph.png")
-    _figsize = (img.shape[1] / 50, img.shape[0] / 100)
-    plt.axis("off")
-    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=_figsize,
-                                   dpi=300, frameon=False, tight_layout=True)
-    for elem in (ax1, ax2):
-        elem.axes.get_xaxis().set_visible(False)
-        elem.axes.get_yaxis().set_visible(False)
-    print(fig.get_size_inches())
-    ax1.imshow(img)
-    ax2.imshow(mpimg.imread("g41DigraphProgress8.png"))
+    num = 8 # 0-8
+    img1 = mpimg.imread("g41DigraphProgress%d.png"%num)
+    img2 = mpimg.imread("incidenceGraph%d.png"%num)
+    _figsize = (max(img1.shape[1],img2.shape[1]) / 100, max(img1.shape[0],img1.shape[0]) / 200)
+    
+    for i in range(num,-1,-1):
+        img1 = mpimg.imread("g41DigraphProgress%d.png"%i)
+        img2 = mpimg.imread("incidenceGraph%d.png"%i)
 
-    plt.savefig("combined")
+        plt.axis("off")
+        fig, (ax1, ax2) = plt.subplots(1, 2, figsize=_figsize,
+                                       dpi=250, frameon=False, tight_layout=False)
+        for elem in (ax1, ax2):
+            elem.axis('off')
+        fig.patch.set_visible(False)
+
+        print(fig.get_size_inches())
+        ax1.imshow(img1)
+        ax2.imshow(img2)
+    
+        plt.savefig("combined%d"%i)
