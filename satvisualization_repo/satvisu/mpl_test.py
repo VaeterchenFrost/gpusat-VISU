ae64c4c580238004c660f8be1de2a20a7442deb0
Martin Röbke <martin.roebke@web.de>
2020-03-11 03:45:24 +0100

    Update mpl_test.py
    Added primal graph in combined plots.
    Each image of size ~1.5MB

--- satvisualization_repo/satvisu/mpl_test.py
+++ satvisualization_repo/satvisu/mpl_test.py
@@ -9,25 +9,29 @@ import matplotlib.image as mpimg
 import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
-    num = 8  # 0-8
-    img1 = mpimg.imread("g41DigraphProgress%d.png" % num)
-    img2 = mpimg.imread("incidenceGraph%d.png" % num)
-    _figsize = (max(img1.shape[1], img2.shape[1]) /
-                100, max(img1.shape[0], img1.shape[0]) / 200)
+    num = 11  # 0-8
+    img1 = mpimg.imread(r"results/g41DigraphProgress%d.png" % num)
+    img2 = mpimg.imread(r"results/incidenceGraph%d.png" % num)
+    img3 = mpimg.imread(r"results/primalGraph%d.png" % num)
+    _figsize = ((img1.shape[1]+ img2.shape[1]+ img3.shape[1]) /
+                300*2.54, max(img1.shape[0], img2.shape[0], img3.shape[0]) /
+                300*2.54)
 
-    for i in range(num, -1, -1):
-        img1 = mpimg.imread("g41DigraphProgress%d.png" % i)
-        img2 = mpimg.imread("incidenceGraph%d.png" % i)
+    for i in range(num, 0, -1):
+        img1 = mpimg.imread(r"results/g41DigraphProgress%d.png" % i)
+        img2 = mpimg.imread(r"results/incidenceGraph%d.png" % i)
+        img3 = mpimg.imread(r"results/primalGraph%d.png" % i)
 
         plt.axis("off")
-        fig, (ax1, ax2) = plt.subplots(1, 2, figsize=_figsize,
-                                       dpi=250, frameon=False, tight_layout=False)
-        for elem in (ax1, ax2):
+        fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=_figsize,
+                                       dpi=300, frameon=False, tight_layout=False)
+        for elem in (ax1, ax2, ax3):
             elem.axis('off')
         fig.patch.set_visible(False)
 
         print(fig.get_size_inches())
         ax1.imshow(img1)
         ax2.imshow(img2)
+        ax3.imshow(img3)
 
-        plt.savefig("combined%d" % i)
+        plt.savefig(r"results/combined%d" % i)
