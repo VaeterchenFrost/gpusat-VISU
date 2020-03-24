994634e1439b9f0e4e289a38bcfe1093c3a00be9
Martin Röbke <martin.roebke@web.de>
2020-03-24 17:00:02 +0100

    Simplify folderhandling and figsize
    Now checks if non existing folders will be created.
    Simplified figsize to be
    _figsize = (25,15)
    until a better generic formula is found.

--- satvisualization_repo/satvisu/mpl_test.py
+++ satvisualization_repo/satvisu/mpl_test.py
@@ -10,31 +10,43 @@ import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
 
-    readfolder = "results31Test\\"
-    writefolder = "results\\"
-    num = 6
+    prependFolder = False  # if true: prepend readfolder-str before the filename
+    readfolder = "example41/"
+    writefolder = "results41/"
+    num = 11
 
     img1 = mpimg.imread(readfolder + "g41DigraphProgress%d.png" % num)
     img2 = mpimg.imread(readfolder + "incidenceGraph%d.png" % num)
     img3 = mpimg.imread(readfolder + "primalGraph%d.png" % num)
-    _figsize = ((img1.shape[1] + img2.shape[1] + img3.shape[1]) / 300 * 2.54,
-                max(img1.shape[0], img2.shape[0], img3.shape[0]) / 300 * 2.54)
-
+    # _figsize = ((img1.shape[0] + img2.shape[0] + img3.shape[0]) / 300 * 2.54,
+    #             max(img1.shape[1], img2.shape[1], img3.shape[1]) / 300 * 2.54)
+    _figsize = (25,15)
     for i in range(num, 0, -1):
-        img1 = mpimg.imread(readfolder + "g41DigraphProgress%d.png" % i)
-        img2 = mpimg.imread(readfolder + "incidenceGraph%d.png" % i)
-        img3 = mpimg.imread(readfolder + "primalGraph%d.png" % i)
+        imgDi = mpimg.imread(readfolder + "g41DigraphProgress%d.png" % i)
+        imgIn = mpimg.imread(readfolder + "incidenceGraph%d.png" % i)
+        imgPr = mpimg.imread(readfolder + "primalGraph%d.png" % i)
 
         plt.axis("off")
         fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=_figsize,
-                                            dpi=300, frameon=False, tight_layout=False)
+                                            dpi=250, frameon=False, tight_layout=True)
         for elem in (ax1, ax2, ax3):
             elem.axis('off')
         fig.patch.set_visible(False)
 
         print(fig.get_size_inches())
-        ax1.imshow(img1)
-        ax2.imshow(img2)
-        ax3.imshow(img3)
+        ax1.imshow(imgDi)
+        ax2.imshow(imgPr)
+        ax3.imshow(imgIn)
 
+        savefigpath= (writefolder +
+                readfolder.replace(
+                    '/',
+                    '_') +
+                "combined%d" %
+                i) if prependFolder else (writefolder + "combined%d" % i)
+        print(savefigpath)
+        import os
+        if not os.path.isdir(writefolder):
+            os.makedirs(writefolder)
+        
         plt.savefig(writefolder + "combined%d" % i)
