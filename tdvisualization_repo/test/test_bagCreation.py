ba20eddeae3aeea5b0f207a520f0bfbf28412853
Martin Röbke <martin.roebke@web.de>
2020-04-20 13:09:41 +0200

    Update test_bagCreation.py
    Rewrote Testing to be used directly or with python -m unittest in the test directory.
    Created the class TestSolutionNode holding all previous tests
    Rewrote them to use the unittest.TestCase methods for assertion.

--- tdvisualization_repo/test/test_bagCreation.py
+++ tdvisualization_repo/test/test_bagCreation.py
@@ -1,8 +1,10 @@
-""" Testing graphvizSatVisu.py
+""" Testing visualization.py
 """
 
+import unittest
+from tdvisu.visualization import Visualization
 
-import satvisu.graphvizSatVisu as gv
+
 SOLUTIONTABLE1 = [["id", "0"], ["v1", "1"],
                   ["v2", "2"], ["v3", "4"],
                   ["nSol", "0"]]
@@ -10,50 +12,57 @@ SOLUTIONTABLE1 = [["id", "0"], ["v1", "1"],
 SOLUTIONTABLEINT = [["id", 0], ["v1", 1],
                     ["v2", 2], ["v3", 4],
                     ["nSol", 0]]
+
 SOLUTIONTABLEFLOAT = [["id", 0.1], ["v1", 1.],
                       ["v2", 2.2222], ["v3", 4.4],
                       ["nSol", 0.1]]
 
 
-def test_solutionNodeEmpty():
-    result = gv.solutionNode([])
-    assert result == "{empty}"
+class TestSolutionNode(unittest.TestCase):
+    def test_solutionNodeEmpty(self):
+        result = Visualization.solutionNode([])
+        self.assertEqual(result, "{empty}")
+
+    def test_solutionNodeEmptyTopLabel(self):
+        result = Visualization.solutionNode([], "top")
+        self.assertEqual(result, "{top|empty}")
+
+    def test_solutionNodeEmptyBottomLabel(self):
+        result = Visualization.solutionNode([], "", "bottom")
+        self.assertEqual(result, "{empty|bottom}")
+
+    def test_solutionNodeTranspose(self):
+        result = Visualization.solutionNode(
+            SOLUTIONTABLE1, "top", "bottom", transpose=True)
+        self.assertEqual(
+            result, "{top|{{id|v1|v2|v3|nSol}|{0|1|2|4|0}}|bottom}")
+
+    def test_solutionNodeFullTable(self):
+        result = Visualization.solutionNode(SOLUTIONTABLE1)
+        self.assertEqual(result, "{{{id|0}|{v1|1}|{v2|2}|{v3|4}|{nSol|0}}}")
+
+        result = Visualization.solutionNode(SOLUTIONTABLE1, "top", "bottom")
+        self.assertEqual(
+            result, "{top|{{id|0}|{v1|1}|{v2|2}|{v3|4}|{nSol|0}}|bottom}")
+
+    def test_solutionNodeLine(self):
+        solutionTable = [["id"], ["v1"],
+                         ["v2"], ["v3"],
+                         ["nSol"]]
+        result = Visualization.solutionNode(solutionTable)
+        self.assertEqual(result, "{{{id}|{v1}|{v2}|{v3}|{nSol}}}")
+
+        result = Visualization.solutionNode(solutionTable, "top", "bottom")
+        self.assertEqual(result, "{top|{{id}|{v1}|{v2}|{v3}|{nSol}}|bottom}")
+
+    def test_solutionNodeNumbers(self):
+        result = Visualization.solutionNode(SOLUTIONTABLEINT)
+        self.assertEqual(result, "{{{id|0}|{v1|1}|{v2|2}|{v3|4}|{nSol|0}}}")
+
+        result = Visualization.solutionNode(SOLUTIONTABLEFLOAT)
+        self.assertEqual(
+            result, "{{{id|0.1}|{v1|1.0}|{v2|2.2222}|{v3|4.4}|{nSol|0.1}}}")
 
 
-def test_solutionNodeEmptyTopLabel():
-    result = gv.solutionNode([], "top")
-    assert result == "{top|empty}"
-
-
-def test_solutionNodeEmptyBottomLabel():
-    result = gv.solutionNode([], "", "bottom")
-    assert result == "{empty|bottom}"
-
-
-def test_solutionNodeTranspose():
-    result = gv.solutionNode(SOLUTIONTABLE1, "top", "bottom", transpose=True)
-    assert result == "{top|{{id|v1|v2|v3|nSol}|{0|1|2|4|0}}|bottom}"
-
-
-def test_solutionNodeFullTable():
-    result = gv.solutionNode(SOLUTIONTABLE1)
-    assert result == "{{{id|0}|{v1|1}|{v2|2}|{v3|4}|{nSol|0}}}"
-    result = gv.solutionNode(SOLUTIONTABLE1, "top", "bottom")
-    assert result == "{top|{{id|0}|{v1|1}|{v2|2}|{v3|4}|{nSol|0}}|bottom}"
-
-
-def test_solutionNodeLine():
-    solutionTable = [["id"], ["v1"],
-                     ["v2"], ["v3"],
-                     ["nSol"]]
-    result = gv.solutionNode(solutionTable)
-    assert result == "{{{id}|{v1}|{v2}|{v3}|{nSol}}}"
-    result = gv.solutionNode(solutionTable, "top", "bottom")
-    assert result == "{top|{{id}|{v1}|{v2}|{v3}|{nSol}}|bottom}"
-
-
-def test_solutionNodeNumbers():
-    result = gv.solutionNode(SOLUTIONTABLEINT)
-    assert result == "{{{id|0}|{v1|1}|{v2|2}|{v3|4}|{nSol|0}}}"
-    result = gv.solutionNode(SOLUTIONTABLEFLOAT)
-    assert result == "{{{id|0.1}|{v1|1.0}|{v2|2.2222}|{v3|4.4}|{nSol|0.1}}}"
+if __name__ == '__main__':
+    unittest.main()
