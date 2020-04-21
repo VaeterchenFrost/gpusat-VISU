c2f980f6476eb9001332e406465c57ff171d1b6b
Martin Röbke <martin.roebke@web.de>
2020-04-21 12:53:59 +0200

    Update setup.py
    More information in package setup

--- tdvisualization_repo/setup.py
+++ tdvisualization_repo/setup.py
@@ -2,9 +2,15 @@
 """
 Created on Mon Jan 27 12:31:12 2020
 
-@author: Martin
+@author: Martin Röbke
 """
 
 from setuptools import setup, find_packages
 
-setup(name="tdvisu", packages=find_packages())
+setup(name="tdvisu",
+      version="0.3",
+      description="Visualizing dynamic programming on tree decompositions.",
+      url="https://github.com/VaeterchenFrost/gpusat-VISU",
+      author="Martin Röbke",
+      author_email="Martin.Roebke@mailbox.tu-dresden.de",
+      packages=find_packages())
