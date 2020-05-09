80e9998fa21e06d6b062adaf586f850c8d06caec
Martin Röbke <martin.roebke@web.de>
2020-05-09 18:46:35 +0200

    Updated Tests
    added file information
    added test_svgjoin.py

--- tdvisualization_repo/test/test_bagCreation.py
+++ tdvisualization_repo/test/test_bagCreation.py
@@ -1,8 +1,12 @@
-""" Testing visualization.py
-"""
+# -*- coding: utf-8 -*-
+""" Testing visualization.py"""
 
 import unittest
 from tdvisu.visualization import Visualization
+
+__author__ = "Martin Röbke <martin.roebke@tu-dresden.de>"
+__status__ = "development"
+__date__ = "20 April 2020"
 
 
 SOLUTIONTABLE1 = [["id", "0"], ["v1", "1"],
@@ -20,28 +24,28 @@ SOLUTIONTABLEFLOAT = [["id", 0.1], ["v1", 1.],
 
 class TestSolutionNode(unittest.TestCase):
     def test_solutionNodeEmpty(self):
-        result = Visualization.solutionNode([])
+        result = Visualization.solution_node([])
         self.assertEqual(result, "{empty}")
 
     def test_solutionNodeEmptyTopLabel(self):
-        result = Visualization.solutionNode([], "top")
+        result = Visualization.solution_node([], "top")
         self.assertEqual(result, "{top|empty}")
 
     def test_solutionNodeEmptyBottomLabel(self):
-        result = Visualization.solutionNode([], "", "bottom")
+        result = Visualization.solution_node([], "", "bottom")
         self.assertEqual(result, "{empty|bottom}")
 
     def test_solutionNodeTranspose(self):
-        result = Visualization.solutionNode(
+        result = Visualization.solution_node(
             SOLUTIONTABLE1, "top", "bottom", transpose=True)
         self.assertEqual(
             result, "{top|{{id|v1|v2|v3|nSol}|{0|1|2|4|0}}|bottom}")
 
     def test_solutionNodeFullTable(self):
-        result = Visualization.solutionNode(SOLUTIONTABLE1)
+        result = Visualization.solution_node(SOLUTIONTABLE1)
         self.assertEqual(result, "{{{id|0}|{v1|1}|{v2|2}|{v3|4}|{nSol|0}}}")
 
-        result = Visualization.solutionNode(SOLUTIONTABLE1, "top", "bottom")
+        result = Visualization.solution_node(SOLUTIONTABLE1, "top", "bottom")
         self.assertEqual(
             result, "{top|{{id|0}|{v1|1}|{v2|2}|{v3|4}|{nSol|0}}|bottom}")
 
@@ -49,17 +53,17 @@ class TestSolutionNode(unittest.TestCase):
         solutionTable = [["id"], ["v1"],
                          ["v2"], ["v3"],
                          ["nSol"]]
-        result = Visualization.solutionNode(solutionTable)
+        result = Visualization.solution_node(solutionTable)
         self.assertEqual(result, "{{{id}|{v1}|{v2}|{v3}|{nSol}}}")
 
-        result = Visualization.solutionNode(solutionTable, "top", "bottom")
+        result = Visualization.solution_node(solutionTable, "top", "bottom")
         self.assertEqual(result, "{top|{{id}|{v1}|{v2}|{v3}|{nSol}}|bottom}")
 
     def test_solutionNodeNumbers(self):
-        result = Visualization.solutionNode(SOLUTIONTABLEINT)
+        result = Visualization.solution_node(SOLUTIONTABLEINT)
         self.assertEqual(result, "{{{id|0}|{v1|1}|{v2|2}|{v3|4}|{nSol|0}}}")
 
-        result = Visualization.solutionNode(SOLUTIONTABLEFLOAT)
+        result = Visualization.solution_node(SOLUTIONTABLEFLOAT)
         self.assertEqual(
             result, "{{{id|0.1}|{v1|1.0}|{v2|2.2222}|{v3|4.4}|{nSol|0.1}}}")
 
---
+++ tdvisualization_repo/test/test_svgjoin.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+""" Testing svgjoin.py"""
+
+import unittest
+from tdvisu.svgjoin import append_svg, new_height
+
+__author__ = "Martin Röbke <martin.roebke@tu-dresden.de>"
+__status__ = "development"
+__date__ = "9 May 2020"
+
+class TestSvgJoin(unittest.TestCase):
+    def test_newheight(self):     
+        self.assertEqual(True, True)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
