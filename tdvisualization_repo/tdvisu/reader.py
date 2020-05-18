7b2cdf13e26f2debeedaff1261b87bcfd855569b
Martin Röbke <martin.roebke@web.de>
2020-05-18 23:24:48 +0200

    Create reader.py
    Reader for tw-output of graphs from dpdb.
    
    Needed to reconstruct the input graph for problems like VertexCover

---
+++ tdvisualization_repo/tdvisu/reader.py
@@ -0,0 +1,123 @@
+# -*- coding: utf-8 -*-
+"""Usage like:
+import reader
+r=reader.TwReader()
+inp = r.from_file('gr5.td')
+inp.num_vertices
+inp.num_edges
+inp.edges
+"""
+
+import logging
+import sys
+
+logger = logging.getLogger(__name__)
+
+
+class Reader():
+    @classmethod
+    def from_file(cls, fname):
+        with open(fname, "r") as file:
+            return cls.from_string(file.read())
+
+    @classmethod
+    def from_stream(cls, stream):
+        return cls.from_string(stream.read().decode())
+
+    @classmethod
+    def from_string(cls, string):
+        instance = cls()
+        instance.parse(string)
+        return instance
+
+    def parse(self, string):
+        pass
+
+
+class DimacsReader(Reader):
+    def parse(self, string):
+        self.problem_solution_type = "?"
+        self.format = "?"
+        lines = string.split("\n")
+        body_start = self.preamble(lines)
+        self.store_problem_vars()
+        self.body(lines[body_start:])
+
+    def store_problem_vars(self):
+        pass
+
+    @staticmethod
+    def is_comment(line):
+        return line.startswith("c ") or line == "c"
+
+    def body(self, lines):
+        pass
+
+    def preamble(self, lines):
+        for lineno, line in enumerate(lines):
+            if line.startswith("p ") or line.startswith("s "):
+                line = line.split()
+                self.problem_solution_type = line[0]
+                self.format = line[1]
+                self._problem_vars = line[2:]
+                return lineno + 1
+            elif not line or self.is_comment(line):
+                continue
+            else:
+                logger.warning(
+                    "Invalid content in preamble at line %d: %s", lineno, line)
+        logger.error("No type found in DIMACS file!")
+        sys.exit(1)
+
+
+def _add_directed_edge(edges, adjacency_list, vertex1, vertex2):
+    if vertex1 in adjacency_list:
+        adjacency_list[vertex1].append(vertex2)
+    else:
+        adjacency_list[vertex1] = [vertex2]
+    edges.append((vertex1, vertex2))
+
+
+class TwReader(DimacsReader):
+    def __init__(self):
+        super().__init__()
+        self.edges = []
+        self.adjacency_list = {}
+
+    def store_problem_vars(self):
+        self.num_vertices = int(self._problem_vars[0])
+        self.num_edges = int(self._problem_vars[1])
+
+    def body(self, lines):
+        if self.format != "tw":
+            logger.error("Not a tw file!")
+            sys.exit(1)
+
+        for lineno, line in enumerate(lines):
+            if not line or self.is_comment(line):
+                continue
+
+            line = line.split()
+            if len(line) != 2:
+                logger.warning(
+                    "Expected exactly 2 vertices at line %d, but %d found",
+                    lineno,
+                    len(line))
+            vertex1 = int(line[0])
+            vertex2 = int(line[1])
+
+            _add_directed_edge(
+                self.edges,
+                self.adjacency_list,
+                vertex1,
+                vertex2)
+            _add_directed_edge(
+                self.edges,
+                self.adjacency_list,
+                vertex2,
+                vertex1)
+
+        if len(self.edges) != self.num_edges * 2:
+            logger.warning(
+                "Effective number of edges mismatch preamble (%d vs %d)", len(
+                    self.edges) / 2, self.num_edges)
